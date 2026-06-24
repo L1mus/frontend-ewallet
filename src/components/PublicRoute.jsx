36b8ead8adb5ab598f0c7037ce7e4ee8e0a99dcf
l1mus <ali.firefox@yahoo.co.id>
2026-06-24 07:47:28 +0700

    fix: bug syntax

--- src/components/PublicRoute.jsx
+++ src/components/PublicRoute.jsx
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router";
 
 const PublicRoute = () => {
-  const { isLogin, loginUser } = useSelector((state) => state.authReducer());
+  const { isLogin, loginUser } = useSelector((state) => state.authReducer);
   if (isLogin && loginUser?.has_pin) {
     return <Navigate to="/dashboard" replace />;
   }
